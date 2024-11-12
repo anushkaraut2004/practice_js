14771f1ddcdca37e1c1a7187bf838ca99450b7c6
ANUSHKA RAUT <135863700+anushkaraut2004@users.noreply.github.com>
2024-11-12 21:30:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let Quantity = 6;
+      document.querySelector("#cartSummary").innerText = 'Your Bag has '+ Quantity + ' items';
+
+let Wish = 1 ;
+      document.querySelector("#WishlistSummary").innerText = `Your Wishlist has ${Wish}  items`;
+
+// let remove=0;
+    //   document.querySelector("#cartSummary").innerText = 'Your Bag has '+  + ' items';
+      
